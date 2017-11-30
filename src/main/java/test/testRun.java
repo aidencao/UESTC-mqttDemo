f0c726ec0a75c7da8693f984ff65ec883dbeab18
Aiden <aiden_cao@outlook.com>
2017-11-30 11:12:43 +0800

    no message

--- src/main/java/test/testRun.java
+++ src/main/java/test/testRun.java
@@ -7,11 +7,6 @@ public class testRun {
 	public static void main(String[] args) throws Exception{
 		testClient.init();
 		
-		//模拟发送open
-		/*MqttClient client = testClient.getClient();
-		MqttMessage message = new MqttMessage();
-		message.setPayload("open".getBytes());
-		client.publish("open/001", message);*/
 		testClient.send("open/001", "open");
 	}
 }
